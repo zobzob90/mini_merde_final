926229c5806d9e41128399fb15238c709d4ef23b
Eric Trignau <ertrigna@f6r7s10.clusters.42paris.fr>
2025-06-09 16:14:17 +0200

    modif signal.c

--- src/signal/signal.c
+++ src/signal/signal.c
@@ -6,7 +6,7 @@
 /*   By: ertrigna <ertrigna@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/13 11:51:41 by ertrigna          #+#    #+#             */
-/*   Updated: 2025/05/13 11:52:15 by ertrigna         ###   ########.fr       */
+/*   Updated: 2025/06/09 16:00:45 by ertrigna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,12 +23,12 @@ void	signal_handler(int signal)
 	}
 	else if (signal == SIGQUIT)
 	{
-		write(1, "Quit\n", 5);
+		return ;
 	}
 }
 
 void	set_signal_handlers(void)
 {
 	signal(SIGINT, signal_handler);
-	signal(SIGQUIT, signal_handler);
+	signal(SIGQUIT, SIG_IGN);
 }
