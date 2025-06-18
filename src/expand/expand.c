49498bbb0706223d44e377508a67748df4adefe9
Valentin Deliere <vdeliere@f6r8s16.clusters.42paris.fr>
2025-06-18 11:49:44 +0200

    correction leak double quote vide

--- src/expand/expand.c
+++ src/expand/expand.c
@@ -33,27 +33,56 @@ static int	contains_expand_chars(const char *str)
 static char	*expand_token(char *token, t_shell *shell)
 {
 	char	*res;
+	char	*tmp;
 	int		i;
 	int		len;
 
 	len = ft_strlen(token);
 	res = ft_calloc(1, 1);
+	tmp = NULL;
 	if (!res)
 		return (NULL);
 	i = 0;
 	while (i < len)
 	{
 		if (token[i] == '\'')
-			res = join_literal(res, token, &i);
+		{
+			tmp = join_literal(tmp, token, &i);
+			if (!tmp)
+			{
+				free(res);
+				return (NULL);
+			}
+		}
 		else if (token[i] == '\"')
-			res = join_double_quote(res, token, &i, shell);
+		{
+			tmp = join_double_quote(tmp, token, &i, shell);
+			if (!tmp)
+			{
+				free(res);
+				return (NULL);
+			}
+		}
 		else if (token[i] == '$' && token[i + 1]
 			&& (ft_isalnum(token[i + 1]) || token[i + 1] == '?'))
-			res = join_dollar(res, token, &i, shell);
+		{
+			tmp = join_dollar(tmp, token, &i, shell);
+			if (!tmp)
+			{
+				free(res);
+				return (NULL);
+			}
+		}
 		else
-			res = join_char(res, token[i++]);
-		if (!res)
-			return (NULL);
+		{
+			tmp = join_char(tmp, token[i++]);
+			if (!tmp)
+			{
+				free(res);
+				return (NULL);
+			}
+		}
+		res = tmp;
 	}
 	return (res);
 }
